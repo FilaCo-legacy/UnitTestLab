3e437899d9d24e3ceb7c6266dcc784b4482960c0
FilaCo <dvchizha@edu.hse.ru>
2019-09-20 04:56:34 +0500

    Добавил новый проект и два основных класса для него

---
+++ QueensProblem/Cell.h
@@ -0,0 +1,31 @@
+#pragma once
+enum Verticals{
+	A = 1,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H
+};
+
+enum Horizontals {
+	ONE = 1,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT
+};
+
+struct Cell
+{
+	Verticals x;
+	Horizontals y;
+
+	Cell(const Verticals x, const Horizontals y): x(x), y(y){}
+};
+
